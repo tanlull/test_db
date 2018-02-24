785f56c3a8e6d512e34fa8be331d0b4a2fc2cc9b
Tanya <tanlull@gmail.com>
2018-02-24 12:58:11 +0700

    add reate user

---
+++ 02 -- Create 10 User.sql
@@ -0,0 +1,23 @@
+CREATE user user0@'%' IDENTIFIED BY 'user0';
+GRANT select ON hr.* To 'user0';
+CREATE user user1@'%' IDENTIFIED BY 'user1';
+GRANT select ON hr.* To 'user1';
+CREATE user user2@'%' IDENTIFIED BY 'user2';
+GRANT select ON hr.* To 'user2';
+CREATE user user3@'%' IDENTIFIED BY 'user3';
+GRANT select ON hr.* To 'user3';
+CREATE user user4@'%' IDENTIFIED BY 'user4';
+GRANT select ON hr.* To 'user4';
+CREATE user user5@'%' IDENTIFIED BY 'user5';
+GRANT select ON hr.* To 'user5';
+CREATE user user6@'%' IDENTIFIED BY 'user6';
+GRANT select ON hr.* To 'user6';
+CREATE user user7@'%' IDENTIFIED BY 'user7';
+GRANT select ON hr.* To 'user7';
+CREATE user user8@'%' IDENTIFIED BY 'user8';
+GRANT select ON hr.* To 'user8';
+CREATE user user9@'%' IDENTIFIED BY 'user9';
+GRANT select ON hr.* To 'user9';
+
+
+flush privileges;
